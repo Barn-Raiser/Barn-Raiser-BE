1bd4e9aa96d790744851f14d82ef988fd0d28b84
Aliya Merali <merali4@gmail.com>
2021-08-28 11:39:51 -0600

    Install graphql, setup graphiql in routes and config

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,35 @@
               status: 1
             )
 end
+
+3.times do
+  Need.create(
+              point_of_contact: Faker::Internet.email,
+              title: Faker::Lorem.sentence(word_count:3),
+              description: Faker::Lorem.paragraph(sentence_count:3),
+              start_time: Time.now,
+              end_time: Time.now + 1,
+              street_address: Faker::Address.street_address,
+              city: Faker::Address.city,
+              state: Faker::Address.state_abbr,
+              zip_code: Faker::Address.zip_code,
+              supporters_needed: Faker::Number.number(digits: 2),
+              status: 0
+            )
+end
+
+2.times do
+  Need.create(
+              point_of_contact: Faker::Internet.email,
+              title: Faker::Lorem.sentence(word_count:3),
+              description: Faker::Lorem.paragraph(sentence_count:3),
+              start_time: Time.now,
+              end_time: Time.now + 1,
+              street_address: Faker::Address.street_address,
+              city: Faker::Address.city,
+              state: Faker::Address.state_abbr,
+              zip_code: Faker::Address.zip_code,
+              supporters_needed: Faker::Number.number(digits: 2),
+              status: 2
+            )
+end
