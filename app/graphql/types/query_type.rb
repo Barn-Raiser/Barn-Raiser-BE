9bdef3cda31c781b56ce431db83b8ee8ade563cf
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-09-02 17:49:58 -0600

    add formating for sake of consitency

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -22,7 +22,7 @@ module Types
       Need.find(id)
     end
 
-
+    # Set up + define field for all categoies query
     field :allCategories, [Types::CategoryType], null: false
     def allCategories
       Category.all
