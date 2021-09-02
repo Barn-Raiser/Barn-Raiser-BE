88096f3792dc799290a34fa798d337e0230f05d2
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-09-01 21:32:19 -0600

    make categories in data base

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_201948) do
+ActiveRecord::Schema.define(version: 2021_09_01_213328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "tag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "need_categories", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "need_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "needs", force: :cascade do |t|
     t.string "point_of_contact"
