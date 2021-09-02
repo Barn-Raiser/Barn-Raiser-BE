12464a23d0560369f5e4d72be6670a3b11e036d8
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-09-01 22:21:21 -0600

    rebase merge

--- app/models/need.rb
+++ app/models/need.rb
@@ -1,6 +1,9 @@
 class Need < ApplicationRecord
   enum status: {pending: 0, active: 1, expired: 2, rejected: 3}
 
+  has_many :need_category
+  has_many :category, through: :need_category
+
   validates :point_of_contact, :title, :description, :start_time, :end_time, :zip_code, :supporters_needed, :status, presence: true
 
   before_create do
---
+++ app/models/need_category.rb
@@ -0,0 +1,6 @@
+class NeedCategory < ApplicationRecord
+
+  belongs_to :category
+  belongs_to :need
+
+end
