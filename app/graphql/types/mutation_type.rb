a5a1d3c94067601be3a8123dcf81470a7302661b
Aliya Merali <merali4@gmail.com>
2021-09-05 12:34:56 -0600

    Create supporter type, add createSupporter mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_need, mutation: Mutations::CreateNeed
+    field :create_supporter, mutation: Mutations::CreateSupporter
   end
 end
