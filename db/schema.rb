02080029985563ad253cf432684c810378ebd625
Aliya Merali <merali4@gmail.com>
2021-09-05 11:11:26 -0600

    Add supporters table and add/update relationship models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_01_213328) do
+ActiveRecord::Schema.define(version: 2021_09_05_170811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,4 +44,14 @@ ActiveRecord::Schema.define(version: 2021_09_01_213328) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "supporters", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.bigint "need_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["need_id"], name: "index_supporters_on_need_id"
+  end
+
+  add_foreign_key "supporters", "needs"
 end
