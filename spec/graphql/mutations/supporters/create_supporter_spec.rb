b98fe21382dd6666e8b72320a0fd8dab66af998a
Aliya Merali <merali4@gmail.com>
2021-09-05 12:34:12 -0600

    Update happy path spec, add sad path spec for createSupporter

--- spec/graphql/mutations/supporters/create_supporter_spec.rb
+++ spec/graphql/mutations/supporters/create_supporter_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'Create a new supporter for a need', type: :request do
                     {
                     name: "Aliya"
                     email: "supporter@example.com"
-                    needId: #{@need_1.id}
+                    need: #{@need_1.id}
                   }
                   )
                   {
@@ -23,7 +23,13 @@ RSpec.describe 'Create a new supporter for a need', type: :request do
                     id
                     name
                     email
-                    needId
+                    need  {
+                      id
+                      title
+                      pointOfContact
+                      categories
+                        {tag}
+                    }
                   }
                   errors
                 }
@@ -38,7 +44,7 @@ RSpec.describe 'Create a new supporter for a need', type: :request do
                     {
                     name: "Andrew"
                     email: "supporter2@example.com"
-                    needId: #{@need_1.id}
+                    need: #{@need_1.id}
                   }
                   )
                   {
@@ -46,35 +52,48 @@ RSpec.describe 'Create a new supporter for a need', type: :request do
                     id
                     name
                     email
-                    needId
+                    need  {
+                      id
+                      title
+                      pointOfContact
+                      categories
+                        {tag}
+                    }
                   }
                   errors
                 }
                 }
               GQL
 
+
       # Supporter 1 for need 2
       @query_3 =
-              <<~GQL
-                mutation {
-                  createSupporter(input:
-                    {
-                    name: "Aliya"
-                    email: "supporter@example.com"
-                    needId: #{@need_2.id}
-                  }
-                  )
+            <<~GQL
+              mutation {
+                createSupporter(input:
                   {
-                  supporter {
+                  name: "Andrew"
+                  email: "supporter2@example.com"
+                  need: #{@need_2.id}
+                }
+                )
+                {
+                supporter {
+                  id
+                  name
+                  email
+                  need  {
                     id
-                    name
-                    email
-                    needId
+                    title
+                    pointOfContact
+                    categories
+                      {tag}
                   }
-                  errors
                 }
-                }
-              GQL
+                errors
+              }
+              }
+            GQL
     end
 
     it 'creates a new Supporter in the database if all required data is present' do
@@ -83,10 +102,10 @@ RSpec.describe 'Create a new supporter for a need', type: :request do
       output = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
-      expect(output[:data][:createSupporter][:supporter][:needId]).to eq(@need_1.id)
+      expect(output[:data][:createSupporter][:supporter][:need][:id].to_i).to eq(@need_1.id)
 
       expect(Supporter.last.need_id).to eq(@need_1.id)
-      expect(Supporter.last.name).to eq("email")
+      expect(Supporter.last.name).to eq("Aliya")
       expect(Supporter.last.email).to eq("supporter@example.com")
     end
 
@@ -102,42 +121,44 @@ RSpec.describe 'Create a new supporter for a need', type: :request do
     end
   end
 
-  xdescribe 'sad path' do
+  describe 'sad path' do
     before :each do
+      @need_1 = Need.create(title: "a need", description: "a test to see if we can test", point_of_contact: "test1@gmail.com", start_time: "2021-11-24 12:00", end_time: "2021-11-24 14:00", zip_code: "12345", supporters_needed: 12, status: "active")
 
       @incomplete_query =
-              <<~GQL
-                mutation {
-                  createNeed(input:
-                    {
-                    title: "Cleanup our park"
-                    description: "I've noticed a lot of litter in the park lately. I'd like to do a cleanup day with the community."
-                    startTime: "2021-08-31 11:00:00 -600"
-                    city: "Denver"
-                    state: "Colorado"
-                    zipCode: "80218"
-                    supportersNeeded: 15
-                    categories: [#{@cat_1.id}, #{@cat_3.id}]
-                  }
-                  )
-                  {
-                  need {
-                    id
-                    title
-                  }
-                  errors
+          <<~GQL
+            mutation {
+              createSupporter(input:
+                {
+                name: "Andrew"
+                need: #{@need_1.id}
+              }
+              )
+              {
+              supporter {
+                id
+                name
+                email
+                need  {
+                  id
+                  title
+                  pointOfContact
+                  categories
+                    {tag}
                 }
-                }
-              GQL
+              }
+              errors
+            }
+            }
+          GQL
     end
 
     it 'returns an error if required data is missing' do
       post '/graphql', params: {query: @incomplete_query}
       output = JSON.parse(response.body, symbolize_names: true)
 
-      expect(output[:need]).to eq(nil)
-      expect(output[:errors].first[:message]).to eq("Argument 'pointOfContact' on InputObject 'CreateNeedInput' is required. Expected type String!")
-      expect(output[:errors].second[:message]).to eq("Argument 'endTime' on InputObject 'CreateNeedInput' is required. Expected type String!")
+      expect(output[:supporter]).to eq(nil)
+      expect(output[:errors].first[:message]).to eq("Argument 'email' on InputObject 'CreateSupporterInput' is required. Expected type String!")
     end
   end
 end
