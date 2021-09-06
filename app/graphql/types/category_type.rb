591b4e26609e4378b98783eccf25011452b08e5d
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-09-06 16:52:53 -0600

    make tests pass

--- app/graphql/types/category_type.rb
+++ app/graphql/types/category_type.rb
@@ -2,6 +2,7 @@ class Types::CategoryType < Types::BaseObject
   field :id, ID, null: false
   field :tag, String, null: false
   field :needs, [Types::NeedType], null:false
+  field :supporters, [Types::SupporterType], null:false
   field :created_at, GraphQL::Types::ISO8601DateTime, null: false
   field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 end
